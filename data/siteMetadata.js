51e590610d388200cfdd1bccf35e785ca67822dc
Watcharapong Chaidee <ne7shii@gmail.com>
2022-12-04 10:51:40 +0700

    update google analytic

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,7 +24,7 @@ const siteMetadata = {
     plausibleDataDomain: 'ne7shii.me', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-WW15J36403', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
